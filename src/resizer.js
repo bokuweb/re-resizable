9b31b49b5e82fe3da4363b6b5d1330061b79fced
bokuweb <bokuweb12@gmail.com>
2016-05-21 16:08:12 +0900

    Add transform assign

--- src/resizer.js
+++ src/resizer.js
@@ -8,7 +8,7 @@ const styles = {
     width: '100%',
     height: '10px',
     top: '-5px',
-    left: 0,
+    left: '0px',
     cursor: 'row-resize',
   },
   right: {
@@ -22,7 +22,7 @@ const styles = {
     width: '100%',
     height: '10px',
     bottom: '-5px',
-    left: 0,
+    left: '0px',
     cursor: 'row-resize',
   },
   left: {
