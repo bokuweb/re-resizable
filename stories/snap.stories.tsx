919fbda62b714a0a987dc3bfdc0330f4ecafc2a1
Riad Benguella <benguella@gmail.com>
2019-06-03 12:12:25 +0100

    Add the possibility to limit snapping using a snapGap property

---
+++ stories/snap.stories.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Resizable } from '../src';
+import { storiesOf } from '@storybook/react';
+import { style } from './style';
+
+storiesOf('snapping', module)
+    .add('absolute', () => (
+        <Resizable 
+            style={style} 
+            snap={{ x: [100, 300, 450], y: [100, 300, 450] }} 
+            snapGap={20}
+            defaultSize={{ width: 50, height: 50 }}
+        >
+            001
+        </Resizable>
+    ))
+    .add('grid', () => (
+        <Resizable 
+            style={style} 
+            grid={[100,100]} 
+            snapGap={20}
+            defaultSize={{ width: 50, height: 50 }}
+        >
+            001
+        </Resizable>
+    ));
